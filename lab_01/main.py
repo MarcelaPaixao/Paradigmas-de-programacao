1c6f65e76f044a68ea3c75655b1645e8b9700eac
MarcelaPaixao <marcela.paixao@edu.ufes.br>
2025-10-12 21:54:28 -0300

    exercicios até 16

--- lab_01/main.py
+++ lab_01/main.py
@@ -1,3 +1,6 @@
+import random
+
+
 def f1():
     num = int(input())
 
@@ -58,4 +61,113 @@ def f6():
         if(num < min): min = num
         if(num > max): max = num
         
-f6()
+def f7():
+    n = int(input())
+    print('Contagem regressiva:')
+    for i in range(n, -1, -1):
+        print(i)
+    print('FIM!')
+
+def f8():
+    c = float(input())
+
+    k = c + 273
+    f = (c * (9/5)) + 32
+
+    print(f'Celcius: {c}°C')
+    print(f'Kelvin: {k}K')
+    print(f'Fahrenheint: {f}°F')
+
+def f9():
+    mat = []
+
+    for i in range(3):
+        row = []
+        for j in range(3):
+            row.append(int(input()))
+        mat.append(row)
+    
+    print(mat)
+
+def f10():
+    a = int(input())
+    b = int(input())
+    
+    for i in range(a, b+1):
+        primo = True
+        for j in range(2, i):
+            if (i != j and i%j == 0):
+                primo = False
+                break
+        if(primo):
+            print(i)
+
+def f11():
+    print('FAZER')################################
+
+def f12():
+    n = int(input())
+    fat = 1
+
+    for i in range(n,1,-1):
+        fat *= i
+    print(f'{n}! = {fat}')
+
+def f13():
+    pal = input()
+    eh_palindromo = True
+    size = len(pal) - 1
+
+    for i in range(int(size/2) + 1):
+        if pal[i] != pal[size - i]:
+            eh_palindromo = False
+
+    print(f'A palvra é palindromo? {eh_palindromo}')
+
+
+def f14():
+    mat = []
+    n = int(input())
+
+    for i in range(n):
+        row = []
+        for j in range(n):
+            if(i == j): row.append(1)
+            else: row.append(0)
+        mat.append(row)
+    
+    print(mat)
+
+def f15():
+    notas = [int(i) for i in input().split()]
+    pesos = [int(i) for i in input().split()]
+    media = 0
+
+    for i in range(len(notas)):
+        media += notas[i] * pesos[i]
+    
+    soma_pesos = 0
+    for p in pesos: soma_pesos += p
+    
+    media = media/soma_pesos
+    print(media)
+
+def f16():
+    n = random.randint(1, 100)
+    count = 0
+
+    print('Tente adivinhar o número: ')
+    while 1:
+        guess = int(input())
+        count += 1
+
+        if guess == n: 
+            print('PARABÉNS, você venceu!') 
+            break
+        if(guess < n): print('O número é MAIOR que esse!')
+        if(guess > n): print('O número é MENOR que esse!')
+        print('\nTente novamente: ')
+
+    print(f'Qtd de tentativas: {count}')
+    
+f13()
